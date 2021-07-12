5d0eb540a6dd00a0494e2df8fd07f4409e050120
Lhourquin <lhourquin@gmail.com>
2021-07-13 01:34:46 +0200

    ok to get the data api with models, now i start to shox the data in the table, jsut test

---
+++ models/CalendarSearch.js
@@ -0,0 +1,43 @@
+export default class CalendarSearch {
+
+
+  constructor(){
+    this.url = 'http://api.aladhan.com/v1/calendarByCity?country=France&method=2';
+
+  }
+
+  searchByCity (month, year, city){
+
+    return fetch(`${this.url}&month=${month}&year=${year}&city=${city}`)
+          .then(Response => Response.json())
+          .then(obj => obj.data)
+          .then(dataArray => dataArray.map( data => {
+            return {
+              date : {
+                gregorian : data.date.gregorian.date,
+                hijri : data.date.hijri.date
+              },
+              salat : {
+                fajr : data.timings.Fajr,
+                shourouk : data.timings.Sunrise,
+                dhor : data.timings.Dhuhr,
+                asr : data.timings.Asr,
+                maghreb : data.timings.Maghrib,
+                icha : data.timings.Isha,
+                midnight : data.timings.Midnight
+              }
+            
+            }
+          })
+          
+          //dataArray.map(data => console.log(data.date.gregorian.date))
+
+
+        );
+    
+    
+        
+  }
+
+
+}
