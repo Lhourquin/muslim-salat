49e492ec4739a1a2c48f06edc03642dd81b15bba
Lhourquin <lhourquin@gmail.com>
2021-07-14 01:12:06 +0200

    template of calandar and event for show date and timings is done

--- controllers/CalendarSalatOfMounth.js
+++ controllers/CalendarSalatOfMounth.js
@@ -21,6 +21,8 @@ export default class CalendarSalatOfMounth {
 
     renderCalendarOfMonth(){
         let calendarSearch = new CalendarSearch();
+        let template = document.getElementById('DayAndHourRow');
+        let eventListRow = document.getElementById('event-list-row');
 
         let date = new Date();
         let month = date.getMonth() + 1;
@@ -28,8 +30,40 @@ export default class CalendarSalatOfMounth {
 
         calendarSearch.searchByCity(month, year, document.getElementById('city').value)
         .then(function (result){
+
+          eventListRow.innerHTML = '';
+          for(let item of result){
+
+            let cloneTemplate = document.importNode(template.content, true);
+            
+            console.log(item.date.gregorian);
+            console.log(item.salat.fajr);
+            let td = cloneTemplate.querySelectorAll('td');
+
+            td[0].textContent = item.date.gregorian;
+            td[1].textContent = item.salat.fajr;
+            td[2].textContent = item.salat.shourouk;
+            td[3].textContent = item.salat.dhor;
+            td[4].textContent = item.salat.asr;
+            td[5].textContent = item.salat.maghreb;
+            td[6].textContent = item.salat.icha;
+            td[7].textContent = item.salat.midnight;
+
+            eventListRow.appendChild(cloneTemplate);
+
+          }
+
+          /*
                 document.getElementsByTagName('tr')[1].children[0].textContent = result[0].date.gregorian;
                 document.getElementsByTagName('tr')[1].children[1].textContent = result[0].salat.fajr;
+                document.getElementsByTagName('tr')[1].children[2].textContent = result[0].salat.shourouk;
+                document.getElementsByTagName('tr')[1].children[3].textContent = result[0].salat.dhor;
+                document.getElementsByTagName('tr')[1].children[4].textContent = result[0].salat.asr;
+                document.getElementsByTagName('tr')[1].children[5].textContent = result[0].salat.maghreb;
+                document.getElementsByTagName('tr')[1].children[6].textContent = result[0].salat.icha;
+                document.getElementsByTagName('tr')[1].children[7].textContent = result[0].salat.midnight;
+
+          */
              }   
             //console.log(result[0].date.gregorian)
            
